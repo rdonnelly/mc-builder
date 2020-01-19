6af44041534ee1921f6af84281acea8466b35ad0
Ryan Donnelly <ryanjdonnelly@gmail.com>
2020-01-18 23:14:52 -0500

    👨‍🎨add icon and splash

--- ios/MCBuilder/AppDelegate.m
+++ ios/MCBuilder/AppDelegate.m
@@ -27,6 +27,30 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  UIView *loadingView = [[UIView alloc] initWithFrame:rootView.bounds];
+
+  UIImageView* launchBackgroundImageView = [[UIImageView alloc] initWithFrame:loadingView.bounds];
+  [launchBackgroundImageView setImage:[UIImage imageNamed:@"Launch-Background"]];
+  [launchBackgroundImageView setContentMode:UIViewContentModeScaleAspectFill];
+  [loadingView addSubview:launchBackgroundImageView];
+
+  UIImageView* launchImageView = [[UIImageView alloc] initWithFrame:loadingView.bounds];
+  [launchImageView setImage:[UIImage imageNamed:@"Launch"]];
+  [launchImageView setContentMode:UIViewContentModeCenter];
+  [loadingView addSubview:launchImageView];
+
+  UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+  spinner.color = [[UIColor alloc] initWithRed:255.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:1.0];
+  spinner.center = CGPointMake(loadingView.frame.size.width * 0.5, loadingView.frame.size.height * 0.85);
+  [spinner startAnimating];
+  [loadingView addSubview:spinner];
+
+
+  rootView.loadingView = loadingView;
+  rootView.loadingViewFadeDelay = 0.5;
+  rootView.loadingViewFadeDuration = 0.5;
+
   return YES;
 }
 

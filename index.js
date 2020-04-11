8ce5bc8d5f05e4ffde1e602e498b578d1c1ad492
Ryan Donnelly <ryanjdonnelly@gmail.com>
2020-04-11 19:41:54 -0400

    add bugsnag

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
+import { Client } from 'bugsnag-react-native';
 import { enableScreens } from 'react-native-screens';
+
+import { name as appName } from './app.json';
 import App from './src/App';
+
+const bugsnag = new Client('e5c34f880df93151bc4547eade19f10c');
 
 enableScreens();
 

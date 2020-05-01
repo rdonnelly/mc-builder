065c9e071b1ff8c6f97f002251e308c3ca7cf7d6
Ryan Donnelly <ryanjdonnelly@gmail.com>
2020-05-01 16:12:34 -0400

    ✨upgrade dependencies and add dotenv

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
+import { BUGSNAG_API_KEY } from 'react-native-dotenv';
 import { Client } from 'bugsnag-react-native';
 import { enableScreens } from 'react-native-screens';
 
 import { name as appName } from './app.json';
 import App from './src/App';
 
-const bugsnag = new Client('e5c34f880df93151bc4547eade19f10c');
+require('react-native').unstable_enableLogBox();
+
+// eslint-disable-next-line no-unused-vars
+const bugsnag = new Client(BUGSNAG_API_KEY);
 
 enableScreens();
 

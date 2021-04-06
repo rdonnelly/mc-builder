ea93b1aac464b973c60e9f14a97df98cd1255a04
Ryan Donnelly <ryanjdonnelly@gmail.com>
2021-04-05 22:23:00 -0400

    add store typing back to selectors

--- packages/mobile/src/store/selectors.ts
+++ packages/mobile/src/store/selectors.ts
@@ -1,26 +1,28 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-// TODO is is possible to type these selectors during usage?
+import { StoreState } from '@store';
 
 export const selectStoreDeck = createSelector(
-  (state, _deckCode: string) => state.root.decks.entities,
-  (_state, deckCode: string) => deckCode,
+  (state: StoreState, _deckCode: string) => state.root.decks.entities,
+  (_state: StoreState, deckCode: string) => deckCode,
   (decks, deckCode) => decks[deckCode],
 );
 
 export const selectStoreDeckCards = createSelector(
-  (state, _deckDeckCardCodes: string[]) => state.root.deckCards.entities,
-  (_state, deckDeckCardCodes: string[]) => deckDeckCardCodes,
+  (state: StoreState, _deckDeckCardCodes: string[]) =>
+    state.root.deckCards.entities,
+  (_state: StoreState, deckDeckCardCodes: string[]) => deckDeckCardCodes,
   (deckCards, deckDeckCardCodes) =>
     deckDeckCardCodes.map((deckCardCode) => deckCards[deckCardCode]),
 );
 
 export const selectStoreDeckCard = createSelector(
-  (state, _deckCode: string, _cardCode: string) => state.root.decks.entities,
-  (state, _deckCode: string, _cardCode: string) =>
+  (state: StoreState, _deckCode: string, _cardCode: string) =>
+    state.root.decks.entities,
+  (state: StoreState, _deckCode: string, _cardCode: string) =>
     state.root.deckCards.entities,
-  (_state, deckCode: string, _cardCode: string) => deckCode,
-  (_state, _deckCode: string, cardCode: string) => cardCode,
+  (_state: StoreState, deckCode: string, _cardCode: string) => deckCode,
+  (_state: StoreState, _deckCode: string, cardCode: string) => cardCode,
   (decks, deckCards, deckCode, cardCode) => {
     const deckDeckCardCodes = decks[deckCode]?.deckCardCodes;
     return deckDeckCardCodes

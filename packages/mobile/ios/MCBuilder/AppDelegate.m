592dd4490de166d9cf6913bf404ef83c9b616a39
Ryan Donnelly <ryan.donnelly@detroitlabs.com>
2021-04-08 19:43:32 -0400

    redo ios

--- packages/mobile/ios/MCBuilder/AppDelegate.m
+++ packages/mobile/ios/MCBuilder/AppDelegate.m
@@ -2,10 +2,7 @@
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTLinkingManager.h>
 #import <React/RCTRootView.h>
-
-#import "RNBootSplash.h"
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -39,41 +36,24 @@ static void InitializeFlipper(UIApplication *application) {
                                                    moduleName:@"MCBuilder"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  if (@available(iOS 13.0, *)) {
+      rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+      rootView.backgroundColor = [UIColor whiteColor];
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-
-  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
-
-  UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-  spinner.color = [[UIColor alloc] initWithRed:52.0/255.0 green:73.0/255.0 blue:94.0/255.0 alpha:1.0];
-  spinner.center = CGPointMake(rootView.loadingView.frame.size.width * 0.5, rootView.loadingView.frame.size.height * 0.85);
-  [spinner startAnimating];
-  [rootView.loadingView addSubview:spinner];
-
   return YES;
-}
-
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
-{
-  return [RCTLinkingManager application:app openURL:url options:options];
-}
-
-- (BOOL)application:(UIApplication *)app continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable)) restorationHandler
-{
-  return [RCTLinkingManager application:app
-                   continueUserActivity:userActivity
-                     restorationHandler:restorationHandler];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"packages/mobile/index" fallbackResource:nil];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  projectRoot: path.resolve(__dirname, '../../'),
   watchFolders: [path.resolve(__dirname, '../../')],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },

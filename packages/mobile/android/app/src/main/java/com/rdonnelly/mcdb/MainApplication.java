0b66d1f0d73b55ecd931ea83b8d77ff300410d58
Ryan Donnelly <ryan.donnelly@detroitlabs.com>
2021-04-09 17:41:20 -0400

    redo android

--- packages/mobile/android/app/src/main/java/com/rdonnelly/mcdb/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/rdonnelly/mcdb/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rdonnelly.mcbuilder;
+package com.rdonnelly.mcdb;
 
 import android.app.Application;
 import android.content.Context;
@@ -31,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected String getJSMainModuleName() {
-          return "packages/mobile/index";
+          return "index";
         }
       };
 
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rdonnelly.mcbuilder.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.rdonnelly.mcdb.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);

4a56f2073786e2c9d3f9f1c8e0d4824af9623db8
Ryan Donnelly <ryanjdonnelly@gmail.com>
2022-02-21 00:07:09 -0500

    [mobile] re-enable freeze

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
 import RNBootSplash from 'react-native-bootsplash';
-// import { enableFreeze } from 'react-native-screens';
+import { enableFreeze } from 'react-native-screens';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
@@ -21,7 +21,7 @@ if (!__DEV__) {
   Bugsnag.start();
 }
 
-// enableFreeze(true);
+enableFreeze(true);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 declare var global: { HermesInternal: null | {} };

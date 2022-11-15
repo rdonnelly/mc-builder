99557e95f19dd6967808234f0d466dc84e9951a7
Ryan Donnelly <ryanjdonnelly@gmail.com>
2022-11-15 16:19:35 -0500

    [mobile] fix react native gesture handler installation for android

--- packages/mobile/android/app/src/main/java/com/rdonnelly/mcbuilder/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/rdonnelly/mcbuilder/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
@@ -22,15 +21,5 @@ public class MainActivity extends ReactActivity {
   protected void onCreate(Bundle savedInstanceState) {
     RNBootSplash.init(this);
     super.onCreate(null);
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
   }
 }
